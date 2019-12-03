80d3a06dc2ba15f0cdad285848b2b83dce8f42e6
rkatarine <rkatarine@mail.ru>
2019-12-03 14:39:11 +0300

    css styles

--- server/router/index.js
+++ server/router/index.js
@@ -7,30 +7,37 @@ module.exports = async (req, res, next) => {
     res.json({
       status: false,
       statusText: "Ожидаемое поле geojson пустое"
-    })
+    });
   }
 
+  if (!geojson.features) {
+    res.json({
+      status: false,
+      statusText: "Ожидаемое поле features пустое"
+    });
+  }
   const filePath = `/../../public/${"data"}.json`;
 
   const isWrite = await writeFile(filePath, JSON.stringify(geojson));
 
-  if (!isWrite.status){
+  if (!isWrite.status) {
     return res.json(isWrite);
   }
 
-  return res.sendFile(path.join(__dirname + '/../../src/html/index.html'));
+  return res.sendFile(path.join(__dirname + "/../../src/html/index.html"));
 };
 
-const writeFile = (filePath, fileData) => new Promise((fulfill, reject) => {
-  const normalizeFilePath = path.join(__dirname, filePath);
-  fs.writeFile(normalizeFilePath, fileData, err => {
-    if (err) {
-      reject(err);
-    }
-    fulfill({
-      status: true,
-      statusText: "Файл успешно записан",
-      data: filePath
+const writeFile = (filePath, fileData) =>
+  new Promise((fulfill, reject) => {
+    const normalizeFilePath = path.join(__dirname, filePath);
+    fs.writeFile(normalizeFilePath, fileData, err => {
+      if (err) {
+        reject(err);
+      }
+      fulfill({
+        status: true,
+        statusText: "Файл успешно записан",
+        data: filePath
+      });
     });
   });
-});
